dee54fa3dc37dc4752f0788e4f7fbc1bb88ad4b4
oitai oita <extramail2042@gmail.com>
2025-08-13 16:12:04 +0600

    Update axios.js

--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -1,3 +1,24 @@
 import axios from "axios";
 
-axios.defaults.baseURL='https://cors-anywhere.herokuapp.com/https://trial00.infy.uk/api/'
+const axios = axios.create({
+  baseURL: 'https://trial00.infy.uk/api',
+  withCredentials: true,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  }
+});
+
+// Add response interceptor
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response && error.response.status === 401) {
+      // Handle unauthorized
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axios
