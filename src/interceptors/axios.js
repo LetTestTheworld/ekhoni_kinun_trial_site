27f22ef71ed49cafe1ff1798c46ac05b4f1e0370
oitai oita <extramail2042@gmail.com>
2025-08-13 16:19:49 +0600

    Update axios.js

--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -1,24 +1,3 @@
 import axios from "axios";
 
-const axios = axios.create({
-  baseURL: 'https://trial00.infy.uk/api',
-  withCredentials: true,
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest'
-  }
-});
-
-// Add response interceptor
-axios.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response && error.response.status === 401) {
-      // Handle unauthorized
-    }
-    return Promise.reject(error);
-  }
-);
-
-export default axios
+axios.defaults.baseURL='https://trial00.infy.uk/api/'
